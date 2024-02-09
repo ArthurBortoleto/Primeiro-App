cd612388e5d198ccfe2b3844cdb54103421b4576
ArthurBortoleto <arthurmarcelbdossantos@gmail.com>
2024-02-09 15:32:53 -0300

    Meu primeiro projeto

---
+++ App.js
@@ -0,0 +1,71 @@
+import { Text, View, Image, StyleSheet, TouchableOpacity, ScrollView } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import Logo from "./src/assets/instagram-logo.png";
+
+export default function App() {
+  return (
+    <View
+      style={{
+        alignItems: "center",
+        flex: 1,
+        marginVertical: 32,
+        marginHorizontal: 16,
+      }}
+    >
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          width: "100%",
+          alignItems: "center",
+        }}
+      >
+        <Image style={{ width: 150, height: 50 }} source={Logo} />
+        <View style={{ flexDirection: "row" }}>
+          <TouchableOpacity>
+            <Feather name="heart" size={32} color="black" />
+          </TouchableOpacity>
+          <TouchableOpacity style={{ marginLeft: 8 }}>
+            <Feather name="message-circle" size={32} color="black" />
+          </TouchableOpacity>
+        </View>
+      </View>
+      <ScrollView contentContainerStyle={{ gap:8 }}
+      horizontal style={{ flexDirection: "row" }}
+      showsHorizontalScrollIndicator={false}>
+        <TouchableOpacity>
+        <Image 
+        style={{ width: 100, height: 100, borderRadius: 50 }}
+        source={{
+          uri: "https://s2-techtudo.glbimg.com/L9wb1xt7tjjL-Ocvos-Ju0tVmfc=/0x0:1200x800/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/q/l/TIdfl2SA6J16XZAy56Mw/canvaai.png"
+        }}
+        />
+        </TouchableOpacity>
+        <TouchableOpacity>
+        <Image
+        style={{ width: 100, height: 100, borderRadius: 50}}
+        source={{
+          uri: "https://t.ctcdn.com.br/JlHwiRHyv0mTD7GfRkIlgO6eQX8=/640x360/smart/i257652.jpeg"
+        }}
+        />
+        </TouchableOpacity>
+        <TouchableOpacity>
+        <Image
+        style={{ width: 100, height: 100, borderRadius: 50 }}
+        source={{
+          uri: "https://www.ufmt.br/ocs/images/phocagallery/galeria2/thumbs/phoca_thumb_l_image03_grd.png"
+        }}
+        />
+        </TouchableOpacity>
+        <TouchableOpacity>
+        <Image
+        style={{ width: 100, height: 100, borderRadius: 50 }}
+        source={{
+          uri: "https://s2-techtudo.glbimg.com/SSAPhiaAy_zLTOu3Tr3ZKu2H5vg=/0x0:1024x609/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2022/c/u/15eppqSmeTdHkoAKM0Uw/dall-e-2.jpg"
+        }}
+        />
+        </TouchableOpacity>
+      </ScrollView>
+    </View>
+  );
+}
